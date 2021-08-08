622a7d6c7714251200c4382b59a06dfc2a3df69a
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-08 15:08:02 +0900

    Heap Sort 6th

--- DS_HeapSort/Heap.h
+++ DS_HeapSort/Heap.h
@@ -4,7 +4,7 @@
 template<typename T>
 class CHeapSort
 {
-public:
+public :
 	CHeapSort()
 	{
 		m_Size = 0;
@@ -20,20 +20,11 @@ private :
 	int m_Size;
 	int m_Capacity;
 	T* m_Array;
-	bool(*m_Func)(const T&, const T&);
-private : 
-	bool SortFunction(const T& Left, const T& Right)
-	{
-		return Left > Right;
-	}
+	bool (*m_Func)(const T&, const T&);
 public :
-	void SetSortFunction(bool (*pFunc)(const T&, const T&))
-	{
-		m_Func = pFunc;
-	}
-public :
-	int size()const { return m_Size;}
-	bool empty() const { return m_Size == 0;}
+	int size() const { return m_Size; }
+	bool empty() const { return m_Size == 0; }
+	void clear() { m_Size = 0; }
 	void push(const T& data)
 	{
 		if (m_Size == m_Capacity)
@@ -51,6 +42,11 @@ public :
 	void pop()
 	{
 		if (empty()) assert(false);
+		else if (m_Size == 1)
+		{
+			m_Size = 0;
+			return;
+		}
 		--m_Size;
 		m_Array[0] = m_Array[m_Size];
 		DeleteHeap(0);
@@ -67,27 +63,35 @@ private :
 			m_Array[Index] = temp;
 			InsertHeap(ParentIndex);
 		}
-
 	}
 	void DeleteHeap(int Index)
 	{
-		int LeftChildIndex = Index * 2 + 1;
+		int LeftChildIndex = (Index * 2) + 1;
 		if (LeftChildIndex >= m_Size)
 			return;
 		int RightChildIndex = LeftChildIndex + 1;
 		int ChildIndex = LeftChildIndex;
-		if (RightChildIndex)
+		if (RightChildIndex < m_Size)
 		{
 			if (m_Func(m_Array[RightChildIndex], m_Array[LeftChildIndex]))
-				ChildIndex = RightChildIndex
+				ChildIndex = RightChildIndex;
 		}
-		if (m_Func(m_Array[RightChildIndex], m_Array[ChildIndex]))
+		if (m_Func(m_Array[ChildIndex], m_Array[Index]))
 		{
-			T temp = m_Array[RightChildIndex];
-			m_Array[RightChildIndex] = m_Array[ChildIndex];
-			m_Array[ChildIndex] = temp;
+			T temp = m_Array[ChildIndex];
+			m_Array[ChildIndex] = m_Array[Index];
+			m_Array[Index] = temp;
 			DeleteHeap(ChildIndex);
- 		}
+		}
 	}
-
+private :
+	static bool SortFunction(const T& Left, const T& Right)
+	{
+		return Left > Right;
+	}
+public :
+	void SetSortFunction(bool(*pFunc)(const T&, const T&))
+	{
+		m_Func = pFunc;
+	}
 };
