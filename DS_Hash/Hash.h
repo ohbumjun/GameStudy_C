1f1c4079366372931cddbc15164e12b7b614cb9f
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-12 16:10:12 +0900

    Hash 3th

--- DS_Hash/Hash.h
+++ DS_Hash/Hash.h
@@ -1,3 +1,62 @@
 #pragma once
 
 #include<string >
+
+class CHash
+{
+public :
+	CHash()
+	{
+
+	}
+	~CHash()
+	{
+
+	}
+private :
+	unsigned __int64 m_HashKey;
+public :
+	template<typename Key>
+	unsigned __int64 GetHash(Key key)
+	{
+		m_HashKey = 0;
+		size_t Length = sizeof(key);
+		unsigned __int64 NewKey = (unsigned __int64)key;
+		for (size_t i = 0; i < Length; i++)
+		{
+			unsigned char data = NewKey & 0xff;
+			m_HashKey += data;
+			NewKey >>= 8;
+		}
+		return m_HashKey;
+	}
+
+	unsigned __int64 GetHash(std::string key)
+	{
+		m_HashKey = 0;
+		size_t Length = key.length();
+		for (size_t i = 0; i < Length; i++)
+		{
+			if (i % 2 == 0)
+				m_HashKey += (unsigned __int64)key[i];
+			else
+				m_HashKey *= (unsigned __int64)key[i];
+		}
+		return m_HashKey;
+	}
+
+	unsigned __int64 GetHash(const char* key)
+	{
+		m_HashKey = 0;
+		size_t Length = strlen(key);
+		for (size_t i = 0; i < Length; i++)
+		{
+			if (i % 2 == 0)
+				m_HashKey += (unsigned __int64)key[i];
+			else
+				m_HashKey *= (unsigned __int64)key[i];
+		}
+		return m_HashKey;
+	}
+
+};
