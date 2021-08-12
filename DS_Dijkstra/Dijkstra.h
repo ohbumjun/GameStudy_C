f1d1bc345c62e8999cecc918fb5369bbfae54e9c
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-12 14:04:20 +0900

    Dijstra 4th

--- DS_Dijkstra/Dijkstra.h
+++ DS_Dijkstra/Dijkstra.h
@@ -1,51 +1,57 @@
 #pragma once
-#include<assert.h>
-#include "Queue.h"
-#include "CDijkstra.h"
-#include "List.h"
+
+#include"Heap.h"
+#include"List.h"
 
 template<typename T>
 class CEdge
 {
 	template<typename T>
-	friend class CDijkstra;
-	template<typename T>
 	friend class CDijkstraNode;
+	template<typename T>
+	friend class CDijkstra;
+public:
+	CEdge() 
+	{
+		m_Node = nullptr;
+	}
+	~CEdge() 
+	{
+	}
 private :
-	CEdge() : m_Node(nullptr){}
-	~CEdge(){}
-private :
-	class CDijkstraNode<T>* m_Node;
 	int m_Cost;
+	class CDijkstraNode<T>* m_Node;
 };
 
 template<typename T>
 class CDijkstraNode
 {
 	template<typename T>
-	friend class CDijstra;
-private :
+	friend class CDijkstra;
+public :
 	CDijkstraNode()
 	{
-
+		m_Size = 0;
+		m_Capacity = 8;
+		m_Visit = false;
+		m_Parent = nullptr;
+		m_EdgeArray = new CEdge<T>*[m_Capacity];
 	}
 	~CDijkstraNode()
 	{
 		for (int i = 0; i < m_Size; i++)
-		{
 			delete m_EdgeArray[i];
-		}
 		delete[] m_EdgeArray;
 	}
 private :
 	int m_Size;
 	int m_Capacity;
-	T** m_EdgeArray;
-	bool m_Visit;
 	int m_Cost;
+	bool m_Visit;
+	T m_Data;
 	CDijkstraNode<T>* m_Parent;
-
-private :
+	CEdge<T>** m_EdgeArray;
+public :
 	void AddEdge(CDijkstraNode<T>* Node, int Cost)
 	{
 		if (m_Size == m_Capacity)
@@ -56,37 +62,20 @@ private :
 			delete[] m_EdgeArray;
 			m_EdgeArray = Array;
 		}
-		CEdge<T> Edge = new CEdge<T>;
-		Edge->m_Cost = Cost;
+		CEdge<T>* Edge = new CEdge<T>;
 		Edge->m_Node = Node;
+		Edge->m_Cost = Cost;
 		m_EdgeArray[m_Size] = Edge;
 		++m_Size;
 	}
-	
 	void Add(CHeap<CDijkstraNode<T>*>& heap)
 	{
 		for (int i = 0; i < m_Size; i++)
 		{
-			// 현재노드의 비용과
-			// Edge의 비용을 더하여
-			// Edge에 연결된 노드의 비용과 비교를 하고
-			// 만약 이 비용이 Edge에 연결된
-			// 노드의 비용보다 더 작다면
-			// 부모노드를 교체해주고, 비용을 교체한다
-			if (m_Cost + m_EdgeArray[i]->m_Cost < m_EdgeArray[i]->m_Node->m_Cost)
-			{
-				m_EdgeArray[i]->m_Node->m_Cost = m_Cost + m_EdgeArray[i]->m_Cost;
-				m_EdgeArray[i]->m_Node->m_Parent = this;
-			}
-
-			// 방문을 위한 노드라면
-			// heap에 넣어서, 연결된 노드를 판단해본다.
-			if (!m_EdgeArray[i]->m_Node->m_Visit)
-			{
-				heap.push(m_EdgeArray[i]->m_Node);
-				m_EdgeArray[i]->m_Node->m_Visit = true;
-			}
-
+			if (m_EdgeArray[i]->m_Node->m_Visit) continue;
+			heap.push(m_EdgeArray[i]->m_Node);
+			m_EdgeArray[i]->m_Node->m_Visit = true;
+			m_EdgeArray[i]->m_Node->m_Parent = this;
 		}
 	}
 };
@@ -95,24 +84,24 @@ template<typename T>
 class CDijkstra
 {
 public :
-	CDijstra()
+	CDijkstra()
 	{
 		m_Size = 0;
-		m_Capacity = 4;
-		m_NodeArray = new CDijkstraNode<T*>[m_Capacity];
+		m_Capacity = 8;
+		m_NodeArray = new CDijkstraNode<T>*[m_Capacity];
 	}
 	~CDijkstra()
 	{
-		for(int i = 0 ; i < m_Size; i++)
+		for (int i = 0; i < m_Size; i++)
 		{
 			delete m_NodeArray[i];
 		}
 		delete[] m_NodeArray;
 	}
 private :
-	CDijkstraNode<T>** m_NodeArray;
 	int m_Size;
 	int m_Capacity;
+	CDijkstraNode<T>** m_NodeArray;
 public :
 	void insert(const T& data)
 	{
@@ -125,13 +114,10 @@ public :
 			m_NodeArray = Array;
 		}
 		CDijkstraNode<T>* Node = new CDijkstraNode<T>;
-
 		Node->m_Data = data;
 		m_NodeArray[m_Size] = Node;
 		++m_Size;
 	}
-
-	// 노드에 edge를 추가해준다
 	void AddEdge(const T& Src, const T& Dest, int Cost)
 	{
 		CDijkstraNode<T>* SrcNode = nullptr;
@@ -141,38 +127,57 @@ public :
 		{
 			if (m_NodeArray[i]->m_Data == Src)
 				SrcNode = m_NodeArray[i];
-			if (m_NodeArray[i]->m_Data == dta)
+			if (m_NodeArray[i]->m_Data == Dest)
 				DestNode = m_NodeArray[i];
 			if (SrcNode && DestNode) break;
 		}
 		if (!SrcNode || !DestNode) return;
-
 		SrcNode->AddEdge(DestNode, Cost);
 		DestNode->AddEdge(SrcNode, Cost);
-	}
 
-	bool Find(CList<T>* ResultList, const T& Start, const T& End)
+	}
+	CDijkstraNode<T>* FindNode(const T& data)
+	{
+		for (int i = 0; i < m_Size; i++)
+		{
+			if (m_NodeArray[i]->m_Data == data) return m_NodeArray[i];
+		}
+		return nullptr;
+	}
+	bool Find(CList<T>*ResultList,const T& Start, const T& End)
 	{
 		CDijkstraNode<T>* StartNode = FindNode(Start);
 		if (!StartNode) return false;
 		CDijkstraNode<T>* EndNode = FindNode(End);
 		if (!EndNode) return false;
 
-		// 모든 노드의 정보를 초기화해준다
+		// 초기화
 		for (int i = 0; i < m_Size; i++)
 		{
+			m_NodeArray[i]->m_Visit = false;
 			m_NodeArray[i]->m_Cost = INT_MAX;
 			m_NodeArray[i]->m_Parent = nullptr;
-			m_NodeArray[i]->m_Visit = false;
 		}
 
-		// 시작노드 비용을
-		// 0으로 처리한다
+		// 시작노드 세팅
 		StartNode->m_Cost = 0;
+		StartNode->m_Visit = true;
+		CHeap<CDijkstraNode<T>*> heap;
+		heap.push(StartNode);
 
+		while (!heap.empty())
+		{
+			CDijkstraNode<T>* Node = heap.top();
+			heap.pop();
+			Node->Add(heap);
+		}
 
-
+		CDijkstraNode<T>* Node = EndNode;
+		while (Node)
+		{
+			ResultList->push_front(Node->m_Data);
+			Node = Node->m_Parent;
+		}
+		return true;
 	}
-
 };
-
