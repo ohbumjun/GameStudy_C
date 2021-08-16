af68e8bbe8d5b3b71b90ea40934991a80ab52ceb
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-16 20:30:59 +0900

    File pt1 3rd

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,16 +1,34 @@
 #include<iostream>
 
+using namespace std;
+
 int main()
 {
 	FILE* FileStream;
-	fopen_s(&FileStream, "Test.txt", "wt");
-	if (FileStream != nullptr)
+	fopen_s(&FileStream, "Test1.txt", "wt");
+	if (FileStream)
 	{
 		fputc('a', FileStream);
-		fputs("aaa\n", FileStream);
-		fprintf(FileStream, "Hello");
+		fputs("æ»≥Á\n", FileStream);
+		fprintf(FileStream, "hello");
 		int Number = 10;
-		fprintf(FileStream, "%d¿‘¥œ¥Ÿ", Number);
+		fprintf(FileStream, "%d æ»≥Á", Number);
+		fclose(FileStream);
+	}
+	fopen_s(&FileStream, "Test1.txt", "rt");
+	if (FileStream)
+	{
+		fseek(FileStream, 0, SEEK_END);
+		int FileSize = ftell(FileStream);
+		fseek(FileStream, 0, SEEK_SET);
+		cout << "FileSize : " << FileSize << endl;
+	
+		char c = fgetc(FileStream);
+		cout << c << endl;
+		char Line[128] = {};
+		fgets(Line, 127, FileStream);
+		cout << Line << endl;
+		fclose(FileStream);
 	}
 
 	return 0;
