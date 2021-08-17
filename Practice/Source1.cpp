fa96549daa483dcccd4c98d9c51ae0c38432c751
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-17 13:50:56 +0900

    File 1,2,3 4th

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,2 +1,32 @@
 #include<iostream>
-#include<>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	std::ofstream write;
+	write.open("hello.txt");
+	if (write.is_open())
+	{
+		char Line[128] = {};
+		strcpy_s(Line, "原原\n");
+		write.write(Line, strlen(Line));
+		strcpy_s(Line, "原原");
+		write.write(Line, strlen(Line));
+	}
+
+	std::ifstream read;
+	read.open("hello.txt");
+	if (read.is_open())
+	{
+		while (!read.eof())
+		{
+			char Line[128] = {};
+			read.getline(Line, 128);
+			cout << Line << endl;
+		}
+	}
+
+	return 0;
+}
