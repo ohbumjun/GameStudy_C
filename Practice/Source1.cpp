39fecaec16e9ec42eb52df66d952492aa0a07d84
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-19 12:33:53 +0900

    command pattern 3rd

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
-#include<vector>
 #include<stack>
+#include<vector>
 
 using namespace std;
+
+// command
+// encapsulate command
+// make command as an object
+// can pass it as parameter, even collection of commands can be in object
+// action is encapsulated inside the command 
 
 class Shape
 {
 public :
-	Shape(){}
 	virtual ~Shape(){}
 public :
 	virtual void Draw() = 0;
@@ -15,7 +20,7 @@ public :
 
 class Rect : public Shape
 {
-public : 
+public :
 	void Draw()
 	{
 		cout << "Draw Rect" << endl;
@@ -35,7 +40,7 @@ struct ICommand
 {
 	virtual void Execute() = 0;
 	virtual bool CanUndo() { return false; }
-	virtual void Undo() = 0;
+	virtual void Undo(){}
 	virtual ~ICommand(){}
 };
 
@@ -43,13 +48,13 @@ class AddCommand : public ICommand
 {
 	vector<Shape*>& v;
 public :
-	AddCommand(vector<Shape*>&a) :v(a){}
-	void Execute()
+	AddCommand(vector<Shape*>&a) : v(a){}
+	void Execute() override
 	{
 		v.push_back(CreateShape());
 	}
-	bool CanUndo() { return true; }
-	void Undo()
+	bool CanUndo() override { return true; }
+	void Undo() override
 	{
 		Shape* p = v.back();
 		v.pop_back();
@@ -58,74 +63,30 @@ public :
 	virtual Shape* CreateShape() = 0;
 };
 
-class AddRectCommand : public AddCommand
-{
-public :
-	AddRectCommand(vector<Shape*>&a) : AddCommand(a){}
-	virtual Shape* CreateShape() override
-	{
-		return new Rect;
-	}
-};
-
-class AddCircleCommand : public AddCommand
-{
-public :
-	AddCircleCommand(vector<Shape*>&a):AddCommand(a){}
-	virtual Shape* CreateShape() override
-	{
-		return new Circle;
-	}
-};
-
 class DrawCommand : public ICommand
 {
 	vector<Shape*>& v;
-public :
-	DrawCommand(vector<Shape*>&a):v(a){}
-	void Execute()
+public:
+	DrawCommand(vector<Shape*>& a) : v(a) {}
+	void Execute() override
 	{
 		for (int i = 0; i < v.size(); i++)
 			v[i]->Draw();
 	}
-	bool CanUndo() { return true; }
-	void Undo()
+	bool CanUndo() override { return true; }
+	void Undo() override
 	{
 		system("cls");
 	}
+	virtual Shape* CreateShape() = 0;
 };
 
-// 여러개 명령을 묶어서 사용하는
-// 매크로 명령
 class MacroCommand : public ICommand
 {
-public :
-	// Composite 패턴
-	vector<ICommand*> v;
-public :
-	void AddCommand(ICommand* p) { v.push_back(p); }
-	void Execute()
-	{
-		for (int i = 0; i < v.size(); i++)
-			v[i]->Execute();
-	}
-	void Undo(){}
+
 };
 
 int main()
 {
-	vector<Shape*> v;
-	stack<ICommand*> cmd_stack;
 
-	MacroCommand* p1 = new MacroCommand;
-	p1->AddCommand(new AddRectCommand(v));
-	p1->AddCommand(new DrawCommand(v));
-	p1->Execute();
-
-	MacroCommand* p2 = new MacroCommand;
-	p2->AddCommand(new AddCircleCommand(v));
-	p2->AddCommand(new DrawCommand(v));
-	p2->Execute();
-
-	return 0;
 }
