598e89127ef60fd078794455b0f0fc87d6df2a2d
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-19 09:39:58 +0900

    command pattern 2nd

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,47 +1,131 @@
 #include<iostream>
+#include<vector>
+#include<stack>
 
 using namespace std;
 
-class CParent
+class Shape
 {
-public:
-	CParent()
+public :
+	Shape(){}
+	virtual ~Shape(){}
+public :
+	virtual void Draw() = 0;
+};
+
+class Rect : public Shape
+{
+public : 
+	void Draw()
 	{
-	}
-	~CParent()
-	{
-	}
-public:
-	virtual void Outputpure1() = 0;
-	virtual void Outputpure2() = 0
-	{
-		cout << "parent out 2" << endl;
+		cout << "Draw Rect" << endl;
 	}
 };
 
-class CChild : public CParent
+class Circle : public Shape
 {
-public:
-	CChild()
+public :
+	void Draw()
 	{
+		cout << "Draw Circle" << endl;
+	}
+};
 
-	}
-	~CChild()
-	{
+struct ICommand
+{
+	virtual void Execute() = 0;
+	virtual bool CanUndo() { return false; }
+	virtual void Undo() = 0;
+	virtual ~ICommand(){}
+};
 
-	}
-	virtual void Outputpure1() override
+class AddCommand : public ICommand
+{
+	vector<Shape*>& v;
+public :
+	AddCommand(vector<Shape*>&a) :v(a){}
+	void Execute()
 	{
+		v.push_back(CreateShape());
 	}
-	virtual void Outputpure2() override
+	bool CanUndo() { return true; }
+	void Undo()
 	{
-		cout << "child out 2" << endl;
+		Shape* p = v.back();
+		v.pop_back();
+		delete p;
 	}
+	virtual Shape* CreateShape() = 0;
+};
+
+class AddRectCommand : public AddCommand
+{
+public :
+	AddRectCommand(vector<Shape*>&a) : AddCommand(a){}
+	virtual Shape* CreateShape() override
+	{
+		return new Rect;
+	}
+};
+
+class AddCircleCommand : public AddCommand
+{
+public :
+	AddCircleCommand(vector<Shape*>&a):AddCommand(a){}
+	virtual Shape* CreateShape() override
+	{
+		return new Circle;
+	}
+};
+
+class DrawCommand : public ICommand
+{
+	vector<Shape*>& v;
+public :
+	DrawCommand(vector<Shape*>&a):v(a){}
+	void Execute()
+	{
+		for (int i = 0; i < v.size(); i++)
+			v[i]->Draw();
+	}
+	bool CanUndo() { return true; }
+	void Undo()
+	{
+		system("cls");
+	}
+};
+
+// 여러개 명령을 묶어서 사용하는
+// 매크로 명령
+class MacroCommand : public ICommand
+{
+public :
+	// Composite 패턴
+	vector<ICommand*> v;
+public :
+	void AddCommand(ICommand* p) { v.push_back(p); }
+	void Execute()
+	{
+		for (int i = 0; i < v.size(); i++)
+			v[i]->Execute();
+	}
+	void Undo(){}
 };
 
 int main()
 {
-	CChild* ch = new CChild;
-	ch->Outputpure2();
+	vector<Shape*> v;
+	stack<ICommand*> cmd_stack;
+
+	MacroCommand* p1 = new MacroCommand;
+	p1->AddCommand(new AddRectCommand(v));
+	p1->AddCommand(new DrawCommand(v));
+	p1->Execute();
+
+	MacroCommand* p2 = new MacroCommand;
+	p2->AddCommand(new AddCircleCommand(v));
+	p2->AddCommand(new DrawCommand(v));
+	p2->Execute();
+
 	return 0;
 }
