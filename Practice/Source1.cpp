9692a39034e403841885480194e880a5b5c1b137
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-20 14:24:23 +0900

    Decorator 3rd

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,70 +1,66 @@
 #include<iostream>
-#include<string>
 #include<vector>
+#include<string>
 
 using namespace std;
 
-class BaseMenu
+struct MilkShake
 {
-private :
-	string m_Name;
-public :
-	BaseMenu(string n):m_Name(n){}
-public :
-	string getName() { return m_Name; }
-	virtual void command() = 0;
+	virtual string Serve() = 0;
+	virtual int Price() = 0;
 };
 
-class PopUpMenu : public BaseMenu
+class BaseMilkShake : public MilkShake
 {
-	vector<BaseMenu*>v;
 public :
-	PopUpMenu(string name):BaseMenu(name){}
+	BaseMilkShake(){}
+	~BaseMilkShake(){}
 public :
-	void addMenu(BaseMenu* menu) { v.push_back(menu); }
-	virtual void command() override
-	{
-		int sz = v.size();
-		while (true)
-		{
-			system("cls");
-			for (int i = 0; i < sz; i++)
-			{
-				cout << i+1 << ". " << v[i]->getName() << endl;
-			}
-			cout << sz + 1 << ". 상위메뉴로" << endl;
-			cout << "메뉴를 선택하세요 : " ;
-			int _Menu;
-			cin >> _Menu;
-			if (_Menu <= 0 || _Menu > sz + 1) continue;
-			if (_Menu == sz + 1) break;
-			v[_Menu - 1]->command();
-		}
-	}
+	string Serve() { return "MilkShake"; }
+	int Price() { return 30; }
 };
 
-class MenuItem : public BaseMenu
+class MilkShakeDecorator : public MilkShake
+{
+protected :
+	MilkShake* base;
+public :
+	MilkShakeDecorator(MilkShake* b):base(b)
+	{
+	}
+public :
+	virtual string Serve() { return base->Serve(); }
+	virtual int Price() { return base->Price(); }
+};
+
+class MangoMilkShakeDecorator : public MilkShakeDecorator
 {
 public :
-	MenuItem(string name) : BaseMenu(name){}
+	MangoMilkShakeDecorator(MilkShake* b):MilkShakeDecorator(b){}
 public :
-	virtual void command() override
-	{
-		cout << getName() << endl;
-	}
+	virtual string Serve() { return base->Serve() + " with Mango"; }
+	virtual int Price() { return base->Price() + 40; }
+};
+
+class VanillaMilkShakeDecorator : public MilkShakeDecorator
+{
+public :
+	VanillaMilkShakeDecorator(MilkShake* b): MilkShakeDecorator(b){}
+public :
+	virtual string Serve() { return base->Serve() + " with Vanilla"; }
+	virtual int Price() { return base->Price() + 80; }
 };
 
 int main()
 {
-	PopUpMenu* menubar = new PopUpMenu("메뉴바");
-	PopUpMenu* p1 = new PopUpMenu("소리설정");
-	PopUpMenu* p2 = new PopUpMenu("화면설정");
-	menubar->addMenu(p2);
-	p1->addMenu(new MenuItem("음량설정"));
-	p1->addMenu(new MenuItem("음질설정"));
-	p2->addMenu(p1);
-	p2->addMenu(new MenuItem("밝기 설정"));
-	menubar->command();
+	BaseMilkShake* base = new BaseMilkShake();
+	cout << base->Serve() << endl;
+	cout << base->Price() << endl;
+	cout << "--------------------" << endl;
+	delete base;
+	MangoMilkShakeDecorator* mangoBase = new MangoMilkShakeDecorator(new BaseMilkShake());
+	cout << mangoBase->Serve() << endl;
+	cout << mangoBase->Price() << endl;
 
 	return 0;
 }
