e40c5acf0854f77b6e39624be80e6daeab96d239
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-20 15:11:21 +0900

    Factory 1st

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,66 +1,91 @@
 #include<iostream>
 #include<vector>
-#include<string>
+#include<map>
 
 using namespace std;
 
-struct MilkShake
-{
-	virtual string Serve() = 0;
-	virtual int Price() = 0;
-};
-
-class BaseMilkShake : public MilkShake
-{
-public :
-	BaseMilkShake(){}
-	~BaseMilkShake(){}
-public :
-	string Serve() { return "MilkShake"; }
-	int Price() { return 30; }
-};
-
-class MilkShakeDecorator : public MilkShake
-{
-protected :
-	MilkShake* base;
-public :
-	MilkShakeDecorator(MilkShake* b):base(b)
-	{
+#define MAKE_SINGLETON(classname)   \
+private :							\
+	classname(){}                   \
+	classname(const classname &);	\
+	void operator=(const classname &);\
+public :							  \
+	static classname& getInstance()   \
+	{								  \
+		static classname instance;    \
+		return instance;			  \
 	}
-public :
-	virtual string Serve() { return base->Serve(); }
-	virtual int Price() { return base->Price(); }
-};
 
-class MangoMilkShakeDecorator : public MilkShakeDecorator
+class Shape
 {
 public :
-	MangoMilkShakeDecorator(MilkShake* b):MilkShakeDecorator(b){}
-public :
-	virtual string Serve() { return base->Serve() + " with Mango"; }
-	virtual int Price() { return base->Price() + 40; }
+	virtual void Draw() = 0;
+	virtual ~Shape(){}
 };
 
-class VanillaMilkShakeDecorator : public MilkShakeDecorator
+class Rect : public Shape 
 {
 public :
-	VanillaMilkShakeDecorator(MilkShake* b): MilkShakeDecorator(b){}
+	void Draw() { cout << "Draw Rect" << endl; }
+	static Shape* CreateShape()
+	{
+		return new Rect;
+	}
+};
+
+class Circle : public Shape
+{
 public :
-	virtual string Serve() { return base->Serve() + " with Vanilla"; }
-	virtual int Price() { return base->Price() + 80; }
+	void Draw() { cout << "Draw Circle" << endl; }
+	static Shape* CreateShape()
+	{
+		return new Circle;
+	}
+};
+
+class ShapeFactory
+{
+	MAKE_SINGLETON(ShapeFactory);
+	typedef Shape* (*CREATOR)();
+	map<int, CREATOR> creator_map;
+public :
+	void RegisterShape(int type, CREATOR creator)
+	{
+		creator_map[type] = creator;
+	}
+	Shape* CreateShape(int type)
+	{
+		Shape* p = 0;
+		if (creator_map[type] != 0)
+		{
+			p = creator_map[type]();
+		}
+		return p;
+	}
 };
 
 int main()
 {
-	BaseMilkShake* base = new BaseMilkShake();
-	cout << base->Serve() << endl;
-	cout << base->Price() << endl;
-	cout << "--------------------" << endl;
-	delete base;
-	MangoMilkShakeDecorator* mangoBase = new MangoMilkShakeDecorator(new BaseMilkShake());
-	cout << mangoBase->Serve() << endl;
-	cout << mangoBase->Price() << endl;
+	vector<Shape*>v;
+	ShapeFactory& factory = ShapeFactory::getInstance();
+	factory.RegisterShape(1, &Rect::CreateShape);
+	factory.RegisterShape(2, &Circle::CreateShape);
+	while (1)
+	{
+		int cmd;
+		cin >> cmd;
 
+		if (cmd > 0 && cmd < 5)
+		{
+			Shape* p = factory.CreateShape(cmd);
+			if (p != 0)
+				v.push_back(p);
+		}
+		else if (cmd == 9)
+		{
+			for (int i = 0; i < v.size(); i++)
+				v[i]->Draw();
+		}
+	}
 	return 0;
 }
