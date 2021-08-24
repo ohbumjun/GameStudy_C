e0b483a3bc94092bfd363b06aafb688101699af2
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-24 14:13:55 +0900

    binary 8th

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,58 +1,107 @@
-#include<iostream>
-#include<cstdlib>
-#include<string>
-#include<cstring>
 
-using namespace std;
+#include <iostream>
+#include <time.h>
+#include "sample.h"
 
-class String
+int Factorial(int Number)
 {
-	char* buff;
-	int* ref;
-	
-public :
-	// 대입 연산자
-	String(const char* c){
-		buff = new char[strlen(c) + 1];
-		strcpy_s(buff, strlen(c) + 1, c);
-		ref = new int(1);
-	}
-	// 복사 생성자
-	String(String& s):buff(s.buff),ref(s.ref){}
-	
-	class CharProxy
-	{
-		String* str;
-		int index;
-	public :
-		CharProxy(String *string,int i) : str(string), index(i){}
-		operator char()
-		{
-			return str->buff[index];
-		}
-		CharProxy operator = (char s)
-		{
-			str->buff[index] = s;
-			return *this;
-		}
-	};
-	CharProxy operator [] (int index)
-	{
-		return CharProxy(this, index);
-	}
-	friend ostream& operator << (ostream& out, String& s)
-	{
-		return out << s.buff << endl;
-	}
-};
+	if (Number == 1)
+		return 1;
+
+	return Number * Factorial(Number - 1);
+}
+
+int FactorialTail(int Number, int Result)
+{
+	if (Number == 1)
+		return Result;
+
+	return FactorialTail(Number - 1, Result * Number);
+}
+
+int FactorialTail(int Number)
+{
+	return FactorialTail(Number, 1);
+}
+
+void Output(const int& Key, const int& Value)
+{
+	std::cout << "Key : " << Key << " Value : " << Value << " => ";
+}
 
 int main()
 {
-	String s1 = "hello";
-	String s2 = s1;
-	cout << s1 << endl;
-	char c = s1[0];
-	s1[0] = 'x';
-	cout << s1 << endl;
+	srand((unsigned int)time(0));
+	rand();
+	//std::cout << FactorialTail(5) << std::endl;
+	CBinaryTree<int, int>	tree;
+
+	//tree.insert(333, 333);
+
+	/*for (int i = 0; i < 20; ++i)
+	{
+		int	Number = rand();
+		tree.insert(Number, Number);
+	}*/
+
+	tree.insert(5, 5);
+	tree.insert(3, 3);
+	tree.insert(1, 1);
+	tree.insert(4, 4);
+	tree.insert(10, 10);
+	tree.insert(8, 8);
+	tree.insert(15, 15);
+	tree.insert(9, 9);
+
+	tree.erase(10);
+
+	CBinaryTree<int, int>::iterator	iter;
+
+	for (iter = tree.begin(); iter != tree.end(); ++iter)
+	{
+		std::cout << "key : " << iter->first << " value : " <<
+			iter->second << std::endl;
+	}
+
+	iter = tree.Find(500);
+
+	if (iter == tree.end())
+		std::cout << "찾는 데이터가 없습니다." << std::endl;
+
+	else
+	{
+		std::cout << "Find Key : " << iter->first << " Find Value : " <<
+			iter->second << std::endl;
+	}
+
+	//tree.clear();
+	tree.erase(5);
+	tree.erase(1);
+	tree.erase(4);
+	//tree.erase(3);
+	//tree.erase(8);
+	//tree.erase(15);
+	//tree.erase(9);
+
+	system("cls");
+	std::cout << "=========== PreOrder ===========" << std::endl;
+	tree.PreOrder(Output);
+	std::cout << std::endl;
+
+	std::cout << "=========== InOrder ===========" << std::endl;
+	tree.InOrder(Output);
+	std::cout << std::endl;
+
+	std::cout << "=========== PostOrder ===========" << std::endl;
+	tree.PostOrder(Output);
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	for (iter = tree.begin(); iter != tree.end(); ++iter)
+	{
+		std::cout << "key : " << iter->first << " value : " <<
+			iter->second << std::endl;
+	}
+
 	return 0;
 }
