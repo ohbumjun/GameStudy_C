99affbeb486209540a74636ba33f480294151872
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-27 19:11:58 +0900

    Visitor 2nd

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,45 +1,4 @@
-#include<iostream>
+/*
 
-using namespace std;
 
-struct IState
-{
-	virtual void run() = 0;
-	virtual void attack() = 0;
-};
-
-class NormalState : public IState
-{
-	void run() { cout << "run" << endl; }
-	void attack() { cout << "attack" << endl; }
-};
-
-class FastState : public IState
-{
-	void run() { cout << "fast run" << endl; }
-	void attack() { cout << "fast attack" << endl; }
-};
-
-class Character
-{
-	IState* state;
-public :
-	void setState(IState* st) { state = st; }
-	void run() { state->run(); }
-	void attack() { state->attack(); }
-};
-
-int main()
-{
-	Character character;
-	NormalState ns;
-	FastState fs;
-	character.setState(&ns);
-	character.attack();
-	character.run();
-	character.setState(&fs);
-	character.attack();
-	character.run();
-	
-	return 0;
-}
+*/
