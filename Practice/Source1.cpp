3d84e656aa49eb2f470b616c68c548dca5b592ba
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-27 15:29:08 +0900

    visitor 1st

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,12 +1,45 @@
 #include<iostream>
-#include<string>
 
-struct Weapon
+using namespace std;
+
+struct IState
 {
-	int m_Attack;
-};
-struct Armor
-{
-	int m_Defence;
+	virtual void run() = 0;
+	virtual void attack() = 0;
 };
 
+class NormalState : public IState
+{
+	void run() { cout << "run" << endl; }
+	void attack() { cout << "attack" << endl; }
+};
+
+class FastState : public IState
+{
+	void run() { cout << "fast run" << endl; }
+	void attack() { cout << "fast attack" << endl; }
+};
+
+class Character
+{
+	IState* state;
+public :
+	void setState(IState* st) { state = st; }
+	void run() { state->run(); }
+	void attack() { state->attack(); }
+};
+
+int main()
+{
+	Character character;
+	NormalState ns;
+	FastState fs;
+	character.setState(&ns);
+	character.attack();
+	character.run();
+	character.setState(&fs);
+	character.attack();
+	character.run();
+	
+	return 0;
+}
