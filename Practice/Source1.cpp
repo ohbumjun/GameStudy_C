98b1117076edbbe5157841cdf3cc12ce14ca6b57
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-27 10:37:28 +0900

    Dijkstra 9th

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -2,40 +2,50 @@
 #include <iostream>
 #include "sample.h"
 
-void OutputNode(const int& data)
-{
-	std::cout << data << " -> ";
-}
-
 int main()
 {
-	CGraph<int>	graph;
+	CDijkstra<const char*>	dijkstra;
 
-	for (int i = 1; i <= 9; ++i)
+	dijkstra.insert("1");
+	dijkstra.insert("2");
+	dijkstra.insert("3");
+	dijkstra.insert("4");
+	dijkstra.insert("5");
+	dijkstra.insert("6");
+	dijkstra.insert("7");
+
+	dijkstra.AddEdge("1", "2", 10);
+	dijkstra.AddEdge("1", "6", 5);
+
+	dijkstra.AddEdge("2", "3", 5);
+	dijkstra.AddEdge("2", "5", 9);
+
+	dijkstra.AddEdge("3", "5", 6);
+	dijkstra.AddEdge("3", "4", 4);
+
+	dijkstra.AddEdge("4", "7", 7);
+
+	dijkstra.AddEdge("5", "6", 7);
+	dijkstra.AddEdge("5", "7", 6);
+
+	CList<const char*>	FindList;
+
+	if (dijkstra.Find(&FindList, "1", "4"))
 	{
-		graph.insert(i);
+		CList<const char*>::iterator	iter;
+
+		for (iter = FindList.begin(); iter != FindList.end(); ++iter)
+		{
+			std::cout << *iter << " -> ";
+		}
+
+		std::cout << std::endl;
 	}
 
-	graph.AddEdge(1, 2);
-	graph.AddEdge(1, 5);
-	graph.AddEdge(1, 7);
-
-	graph.AddEdge(2, 3);
-	graph.AddEdge(5, 6);
-	graph.AddEdge(7, 8);
-
-	graph.AddEdge(3, 4);
-	graph.AddEdge(8, 9);
-
-	std::cout << "============ BFS ============" << std::endl;
-	graph.BFS(OutputNode);
-
-	std::cout << std::endl;
-
-	std::cout << "============ DFS ============" << std::endl;
-	graph.DFS(OutputNode);
-
-	std::cout << std::endl;
+	else
+	{
+		std::cout << "길이 없다" << std::endl;
+	}
 
 	return 0;
 }
--- Practice/sample.h
+++ Practice/sample.h
@@ -1,246 +1,181 @@
-#pragma once
+#pragma once 
+#include "List.h"
+#include "Heap.h"
 
-#include <assert.h>
-#include "Queue.h"
-#include "Stack.h"
-
-template <typename T>
-class CEdge
+template<typename T>
+class CEdge;
+template<typename T>
+class CDijkstraNode
 {
-	template <typename T>
-	friend class CGraph;
-
-	template <typename T>
-	friend class CGraphNode;
-
-private:
-	CEdge()
-	{
-		m_Node = nullptr;
-	}
-
-	~CEdge()
-	{
-	}
-
-private:
-	class CGraphNode<T>* m_Node;
-};
-
-template <typename T>
-class CGraphNode
-{
-	template <typename T>
-	friend class CGraph;
-
-private:
-	CGraphNode()
+	template<typename T>
+	friend class CDijkstra;
+public :
+	CDijkstraNode()
 	{
 		m_Size = 0;
-		m_Capacity = 1;
+		m_Capacity = 8;
 		m_EdgeArray = new CEdge<T>*[m_Capacity];
 		m_Visit = false;
+		m_Cost = INT_MAX;
+		m_Parent = nullptr;
 	}
-
-	~CGraphNode()
+	~CDijkstraNode()
 	{
-		for (int i = 0; i < m_Size; ++i)
-		{
-			delete	m_EdgeArray[i];
-		}
-
-		delete[]	m_EdgeArray;
+		for (int i = 0; i < m_Size; i++)
+			delete m_EdgeArray[i];
+		delete[] m_EdgeArray;
 	}
-
-private:
+private :
 	CEdge<T>** m_EdgeArray;
-	int	m_Size;
-	int	m_Capacity;
-	T		m_Data;
-	bool	m_Visit;
-
-private:
-	void AddEdge(CGraphNode<T>* Node)
+	CDijkstraNode<T>* m_Parent;
+	T m_Data;
+	bool m_Visit;
+	int m_Size;
+	int m_Capacity;
+	int m_Cost;
+public :
+	int size() const { return m_Size; }
+	bool empty() const { return m_Size == 0; }
+	void AddNode(CDijkstraNode<T>* Node, int Cost)
 	{
 		if (m_Size == m_Capacity)
 		{
 			m_Capacity *= 2;
-
 			CEdge<T>** Array = new CEdge<T>*[m_Capacity];
-
 			memcpy(Array, m_EdgeArray, sizeof(CEdge<T>*) * m_Size);
-
-			delete[]	m_EdgeArray;
-
+			delete[] m_EdgeArray;
 			m_EdgeArray = Array;
 		}
-
 		CEdge<T>* Edge = new CEdge<T>;
 		Edge->m_Node = Node;
-
+		Edge->m_Cost = Cost;
 		m_EdgeArray[m_Size] = Edge;
 		++m_Size;
 	}
-
-	void Add(CQueue<CGraphNode<T>*>& queue)
+	void Add(CHeap<CDijkstraNode<T>*>& heap)
 	{
-		for (int i = 0; i < m_Size; ++i)
+		for (int i = 0; i < m_Size; i++)
 		{
-			// Edge로 연결된 노드가 이미 방문한 노드라면 추가를 안한다.
-			if (m_EdgeArray[i]->m_Node->m_Visit)
-				continue;
-
-			queue.push(m_EdgeArray[i]->m_Node);
-
-			m_EdgeArray[i]->m_Node->m_Visit = true;
-		}
-	}
-
-	void Add(CStack<CGraphNode<T>*>& stack)
-	{
-		for (int i = 0; i < m_Size; ++i)
-		{
-			// Edge로 연결된 노드가 이미 방문한 노드라면 추가를 안한다.
-			if (m_EdgeArray[i]->m_Node->m_Visit)
-				continue;
-
-			stack.push(m_EdgeArray[i]->m_Node);
-
-			m_EdgeArray[i]->m_Node->m_Visit = true;
+			if (m_EdgeArray[i]->m_Node->m_Visit) continue;
+			if (m_Cost + m_EdgeArray[i]->m_Cost < m_EdgeArray[i]->m_Node->m_Cost)
+			{
+				m_EdgeArray[i]->m_Node->m_Cost   = m_Cost + m_EdgeArray[i]->m_Cost;
+				m_EdgeArray[i]->m_Node->m_Visit  = true;
+				m_EdgeArray[i]->m_Node->m_Parent = this;
+				heap.push(m_EdgeArray[i]->m_Node);
+			}
 		}
 	}
 };
 
-template <typename T>
-class CGraph
+template<typename T>
+class CEdge
+{
+	template<typename T>
+	friend class CDijkstra;
+	template<typename T>
+	friend class CDijkstraNode;
+public :
+	CEdge()
+	{
+		m_Node = nullptr;
+	}
+private :
+	CDijkstraNode<T>* m_Node;
+	int m_Cost;
+};
+
+
+template<typename T>
+class CDijkstra
 {
 public:
-	CGraph()
+	CDijkstra()
 	{
 		m_Size = 0;
-		m_Capacity = 4;
-
-		m_NodeArray = new CGraphNode<T>*[m_Capacity];
+		m_Capacity = 8;
+		m_NodeArray = new CDijkstraNode<T>*[m_Capacity];
 	}
-
-	~CGraph()
+	~CDijkstra()
 	{
-		for (int i = 0; i < m_Size; ++i)
-		{
-			delete	m_NodeArray[i];
-		}
-
-		delete[]	m_NodeArray;
+		for (int i = 0; i < m_Size; i++)
+			delete m_NodeArray[i];
+		delete[] m_NodeArray;
 	}
-
-private:
-	CGraphNode<T>** m_NodeArray;
-	int		m_Size;
-	int		m_Capacity;
-
-public:
+private :
+	class CDijkstraNode<T>** m_NodeArray;
+	int m_Size;
+	int m_Capacity;
+public :
 	void insert(const T& data)
 	{
 		if (m_Size == m_Capacity)
 		{
 			m_Capacity *= 2;
-
-			CGraphNode<T>** Array = new CGraphNode<T>*[m_Capacity];
-
-			memcpy(Array, m_NodeArray, sizeof(CGraphNode<T>*) * m_Size);
-
-			delete[]	m_NodeArray;
-
+			CDijkstraNode<T>** Array = new CDijkstraNode<T>*[m_Capacity];
+			memcpy(Array, m_NodeArray, sizeof(CDijkstraNode<T>*) * m_Size);
+			delete[] m_NodeArray;
 			m_NodeArray = Array;
 		}
-
-		CGraphNode<T>* Node = new CGraphNode<T>;
-
+		CDijkstraNode<T>* Node = new CDijkstraNode<T>;
 		Node->m_Data = data;
-
 		m_NodeArray[m_Size] = Node;
 		++m_Size;
 	}
-
-	// 노드에 edge를 추가해준다.
-	void AddEdge(const T& Src, const T& Dest)
+	void AddEdge(const T& St, const T& End, int Cost)
 	{
-		CGraphNode<T>* SrcNode = nullptr;
-		CGraphNode<T>* DestNode = nullptr;
+		CDijkstraNode<T>* StNode = nullptr;
+		CDijkstraNode<T>* EdNode = nullptr;
 
-		for (int i = 0; i < m_Size; ++i)
+		for (int i = 0; i < m_Size; i++)
 		{
-			if (m_NodeArray[i]->m_Data == Src)
-				SrcNode = m_NodeArray[i];
-
-			else if (m_NodeArray[i]->m_Data == Dest)
-				DestNode = m_NodeArray[i];
-
-			if (SrcNode && DestNode)
-				break;
+			if (m_NodeArray[i]->m_Data == St)
+				StNode = m_NodeArray[i];
+			if (m_NodeArray[i]->m_Data == End)
+				EdNode = m_NodeArray[i];
+			if (StNode && EdNode) break;
 		}
-
-		if (!SrcNode || !DestNode)
-			return;
-
-		SrcNode->AddEdge(DestNode);
-		DestNode->AddEdge(SrcNode);
+		if (!StNode || !EdNode) return;
+		StNode->AddNode(EdNode, Cost);
+		EdNode->AddNode(StNode, Cost);
 	}
-
-	void BFS(void(*pFunc)(const T&))
+	bool Find(CList<const char*>* ResultList, const T& St, const T& Ed)
 	{
-		if (m_Size == 0)
-			return;
+		CDijkstraNode<T>* StNode = FindNode(St);
+		CDijkstraNode<T>* EdNode = FindNode(Ed);
 
-		// 모든 노드의 방문상태를 false로 만들어준다.
-		for (int i = 0; i < m_Size; ++i)
+		if (!StNode || !EdNode) return false;
+
+		for (int i = 0; i < m_Size; i++)
 		{
-			m_NodeArray[i]->m_Visit = false;
+			m_NodeArray[i]->m_Visit  = false;
+			m_NodeArray[i]->m_Cost   = INT_MAX;
+			m_NodeArray[i]->m_Parent = nullptr;
 		}
 
-		CQueue<CGraphNode<T>*>	queue;
-
-		queue.push(m_NodeArray[0]);
-
-		m_NodeArray[0]->m_Visit = true;
-
-		while (!queue.empty())
+		CHeap<CDijkstraNode<T>*> heap;
+		StNode->m_Visit = true;
+		StNode->m_Cost = 0;
+		heap.push(StNode);
+		while (!heap.empty())
 		{
-			CGraphNode<T>* Node = queue.front();
-			queue.pop();
-
-			Node->Add(queue);
-
-			pFunc(Node->m_Data);
+			CDijkstraNode<T>* Node = heap.top();
+			heap.pop();
+			Node->Add(heap);
 		}
+
+		CDijkstraNode<T>* Node = EdNode;
+		while (Node)
+		{
+			ResultList->push_back(Node->m_Data);
+			Node = Node->m_Parent;
+		}
+		return true;
 	}
-
-	void DFS(void(*pFunc)(const T&))
+	CDijkstraNode<T>* FindNode(const T& Data)
 	{
-		if (m_Size == 0)
-			return;
-
-		// 모든 노드의 방문상태를 false로 만들어준다.
-		for (int i = 0; i < m_Size; ++i)
-		{
-			m_NodeArray[i]->m_Visit = false;
-		}
-
-		CStack<CGraphNode<T>*>	stack;
-
-		stack.push(m_NodeArray[0]);
-
-		m_NodeArray[0]->m_Visit = true;
-
-		while (!stack.empty())
-		{
-			CGraphNode<T>* Node = stack.top();
-			stack.pop();
-
-			Node->Add(stack);
-
-			pFunc(Node->m_Data);
-		}
+		for (int i = 0; i < m_Size; i++)
+			if (m_NodeArray[i]->m_Data == Data) return m_NodeArray[i];
+		return nullptr;
 	}
 };
-
