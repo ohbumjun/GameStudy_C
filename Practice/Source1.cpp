b9a6207e8bfdd83c6e163ccd17cc38b2a95aac3f
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-28 21:38:14 +0900

    template_method 1

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,28 +1,72 @@
 #include<iostream>
-
-template<typename T>
-struct IVisitor;
-
-template<typename T>
-struct IAcceptor
-{
-	virtual void accept(IVisitor*) = 0;
-};
-
-template<typename T>
-
-
-
-template<typename T>
-struct IVisitor
-{
-	virtual void visit(T& a) = 0;
-};
-
+#include<string>
 
 using namespace std;
 
+class Game
+{
+protected :
+	int m_cur_player{ 0 };
+	int m_num_of_players;
+protected :
+	virtual void start() = 0;
+	virtual bool have_winner() = 0;
+	virtual void take_turn() = 0;
+	virtual int get_winner() = 0;
+public :
+	explicit Game(int num_of_players) :
+		m_num_of_players(num_of_players){}
+	void run()
+	{
+		start();
+		while (!have_winner())
+			take_turn();
+		cout << "Player " << get_winner() << " wins\n"  << endl;
+	}
+};
+
+class Chess : public Game
+{
+private:
+	int turns{ 0 }, max_turns{ 10 };
+public :
+	explicit Chess() :Game(2){}
+protected :
+	void start() override
+	{
+		cout << "start chess with " << m_num_of_players << endl;
+	}
+	bool have_winner() override
+	{
+		return turns == max_turns;
+	}
+	void take_turn() override
+	{
+		cout << "Turn " << turns << " taken by player " << m_cur_player << endl;
+		turns++;
+		m_cur_player = (m_cur_player + 1) % m_num_of_players;
+	}
+	int get_winner() override
+	{
+		return m_cur_player;
+	}
+};
+
 int main()
 {
+	Chess chess;
+	chess.run();
+	getchar();
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
