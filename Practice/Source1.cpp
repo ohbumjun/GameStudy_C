dd722629a2bb1d3b1b69f2d0ffbb06d03e96426f
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-29 16:36:26 +0900

    FlyWeight 1st

--- Practice/Source1.cpp
+++ Practice/Source1.cpp
@@ -1,96 +1,47 @@
 #include<iostream>
-#include<vector>
+#include<map>
 #include<string>
 
 using namespace std;
 
-class Client;
-// Mediator
-class Chat
+// FlyWeight :
+// 속성이 동일한 객체들은 공유해서
+// 사용할 수 있게 하자.
+
+// 플라이웨이트 + 팩토리
+
+class Image
 {
-	vector<Client*> m_Clients;
-public :
-	void addClient(Client* client);
-	void chat(Client* client, const string &message);
-	~Chat()
+	string url;
+	Image(string s) : url(s)
 	{
-		vector<Client*>::iterator iter    = m_Clients.begin();
-		vector<Client*>::iterator iterEnd = m_Clients.end();
-		for (; iter != iterEnd; ++iter)
-			delete* iter;
-		cout << "chat deleted" << endl;
+		cout << "Downloading from " << url << endl;
 	}
+public :
+	void Draw() { cout << "Draw Image" << url << endl; }
+	friend class ImageFactory;
 };
 
-// Clinet
-class Client
+class ImageFactory
 {
-private :
-	string m_Name;
-	Chat* m_Chat;
+	map<string, Image*> image_map;
 public :
-	Client(Chat* chat, const string& message);
-	void sendMessage(const string &message);
-	void receiveMessage(Client* client, const string &message);
-	~Client() { cout << m_Name << " deleted" << endl; }
-};
-
-void Chat::addClient(Client* client)
-{
-	m_Clients.push_back(client);
-}
-
-void Chat::chat(Client* client, const string &message)
-{
-	vector<Client*>::iterator iter    = m_Clients.begin();
-	vector<Client*>::iterator iterEnd = m_Clients.end();
-	for (; iter != iterEnd; ++iter)
+	Image* CreateImage(string url)
 	{
-		if ((*iter) != client)
-		{
-			(*iter)->receiveMessage(client, message);
-		}
+		if (image_map[url] == 0)
+			image_map[url] = new Image(url);
+		return image_map[url];
 	}
-}
-
-Client::Client(Chat* chat, const string& message)
-{
-	m_Chat = chat;
-	m_Name = message;
-	chat->addClient(this);
-}
-
-void Client::sendMessage(const string& message)
-{
-	cout << m_Name << "mis sending message : " << message << endl;
-	m_Chat->chat(this, message);
-}
-void Client::receiveMessage(Client* client, const string& message)
-{
-	cout << "From " << client->m_Name << " To " << m_Name << " : " << message << endl;
-}
+};
 
 int main()
 {
-	Chat* chat = new Chat();
+	// 그림을 만드는 공장을 짓고
+	ImageFactory factory;
 
-	Client* cl1 = new Client(chat, "cl1");
-	Client* cl2 = new Client(chat, "cl2");
-	Client* cl3 = new Client(chat, "cl3");
-	Client* cl4 = new Client(chat, "cl4");
-
-	cl1->sendMessage("hello I am cl1");
-	cl2->sendMessage("hello I am cl2");
-	cl3->sendMessage("hello I am cl3");
-	cl4->sendMessage("hello I am cl4");ㅁ
-
-	delete chat;
-	// delete cl1;
-	// delete cl2;
-	// delete cl3;
-	// delete cl4;
-
-	// vector는 메모리 해제가 어떻게 이루어지는 거지 ?
-
-	return 0;
+	// 그림이 필요하면 공장에 요청한다
+	Image* img1 = factory.CreateImage("http://www.naver.com//a.png");
+	img1->Draw();
+	Image* img2 = factory.CreateImage("http://www.naver.com//a.png");
+	img2->Draw();
 }
