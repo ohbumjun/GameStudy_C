87092570919330ccfc7f439b9a9977792e92d3f4
OH BUM JUN <beomjun0638@hanmail.net>
2021-10-26 22:51:17 +0900

    211026 Adapter Pattern

--- Practice/Practice/main.cpp
+++ Practice/Practice/main.cpp
@@ -1,59 +1,56 @@
-// 프록시 패턴
-// 아무리 어려운 문제도 대신할 객체(간접층)을 도입하면 해결할수 있다.
-
-#define _CRT_SECURE_NO_WARNINGS
-
-//문제
-//	=>  참조계수로 동작하는 string 클래스를 생각해봅시다.
-//	=> COW( Copy On Write ) 기법을 활용해서 String을 작성해봅시다
-
-#include<iostream>
-#include<cstdlib>
-#include<string>
-#include<stdio.h>
+#include <iostream>
 
 using namespace std;
 
-class String
+// Adaptee
+class BT
 {
-	int* ref;
-	char* buff;
-public:
-	String(const char* s)
-	{
-		buff = new char[strlen(s) + 1];
-		strcpy(buff, s);
-		ref = new int(1);
-	}
-	String(const String& s) : buff(s.buff), ref(s.ref)
-	{
-		++(*ref);
-	}
-	//[] 연산자 : 객체를 배열처럼 보이게 한다.
-	char& operator[](int index)
-	{
-		cout << "operator []" << endl;
-		return buff[index];
-	}
+public :
+	static void Activate() { cout << "bt activate" << endl; }
+	static void Deactivate() { cout << "bt deactivate" << endl; }
 };
+
+class Wifi
+{
+public:
+	static void Activate() { cout << "Wifi activate" << endl; }
+	static void Deactivate() { cout << "Wifi deactivate" << endl; }
+};
+
+// Target Interface
+class NetWorkController
+{
+public:
+	virtual void Activate() = 0;
+	virtual void Deactivate() = 0;
+};
+
+
+// Specific Classese
+class WifiController : public NetWorkController
+{
+public :
+	virtual void Activate() { Wifi::Activate(); }
+	virtual void Deactivate() { Wifi::Deactivate(); }
+};
+
+class BTController : public NetWorkController
+{
+public:
+	virtual void Activate() { BT::Activate(); }
+	virtual void Deactivate() { BT::Deactivate(); }
+};
+
 int main()
 {
-	// 첫번째 줄은 "대입"연산자
-	// 두번째 줄은 "복사"생성자  
-	String s1 = "hello";
-	String s2 = s1; // 메모리를 잘 생각해보세요. 복사생성자를 보세요
+	NetWorkController* ctrl = new BTController();
+	ctrl->Activate();
+	ctrl->Deactivate();
 
-	char c = s1[0]; // 읽는 코드. 자원은 계속 공유 되어야 한다.
-	//cout << c << endl; // 'h'
-	s1[0] = 'x'; // 쓰는 코드. 자원은 분리되어야 한다.
-	// COW(Copy On Write)
-	// cout << s1 << endl; // "xello"
-	//cout << s2 << endl; // "hello"
+	ctrl = new WifiController();
+	ctrl->Activate();
+	ctrl->Deactivate();
+
+
+	return 0;
 }
-
-
-
-
-
-
-
