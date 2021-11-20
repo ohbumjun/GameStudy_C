a475a63aca34f97e3d217640b68fd03b2e080e24
ohbumjun <beomjun0638@hanmail.net>
2021-11-20 16:49:25 +0900

    C++ Array.h

--- DataStructure/DS_Array/Array.h
+++ DataStructure/DS_Array/Array.h
@@ -1,74 +1,129 @@
 #pragma once
-#include<assert.h>
 
-template<typename T>
+#include <assert.h>
+#include <string.h>
+
+template <typename T>
 class CArray
 {
-public :
-	CArray(){
+public:
+	CArray()
+	{
 		m_Size = 0;
 		m_Capacity = 4;
+
 		m_Array = new T[m_Capacity];
 	}
-	CArray(const CArray& Array)
+
+	CArray(const CArray<T>& Array)
 	{
 		m_Size = Array.m_Size;
 		m_Capacity = Array.m_Capacity;
+
 		m_Array = new T[m_Capacity];
+
 		memcpy(m_Array, Array.m_Array, sizeof(T) * m_Size);
 	}
-	~CArray(){
-		delete[] m_Array;
+
+	~CArray()
+	{
+		delete[]	m_Array;
 	}
-private :
-	int m_Size;
-	int m_Capacity;
+
+private:
 	T* m_Array;
-public :
-	int size() const { return m_Size; }
-	bool empty() const { return m_Size == 0; }
+	int	m_Capacity;	// 실제 배열이 생성된 전체 개수
+	int	m_Size;		// 생성된 배열중 채워진 개수
+
+public:
 	void push_back(const T& data)
 	{
+		// 이미 모든 데이터가 다 채워져 있다면 공간을 늘려준다.
 		if (m_Size == m_Capacity)
 		{
 			m_Capacity *= 2;
+
 			T* Array = new T[m_Capacity];
+
+			// memcpy : 메모리 복사 기능이다.
+			// 2번인자에 들어간 메모리 주소로부터 복사를 시작한다.
+			// 3번인자의 크기만큼을 1번인자의 메모리주소에서 그 크기만큼 채워준다.
 			memcpy(Array, m_Array, sizeof(T) * m_Size);
-			delete[] m_Array;
+
+			// 기존 공간을 제거한다.
+			delete[]	m_Array;
+
 			m_Array = Array;
 		}
+
 		m_Array[m_Size] = data;
 		++m_Size;
 	}
-	void pop()
+
+	void pop_back()
 	{
-		if (empty()) assert(false);
+		if (empty())
+			assert(false);
+
 		--m_Size;
 	}
-	void operator = (const CArray& Array)
+
+	T& front()	const
 	{
+		if (empty())
+			assert(false);
+
+		return m_Array[0];
+	}
+
+	T& back()	const
+	{
+		if (empty())
+			assert(false);
+
+		return m_Array[m_Size - 1];
+	}
+
+	int size()	const
+	{
+		return m_Size;
+	}
+
+	int capacity()	const
+	{
+		return m_Capacity;
+	}
+
+	bool empty()	const
+	{
+		return m_Size == 0;
+	}
+
+	void operator = (const CArray<T>& Array)
+	{
+		delete[]	m_Array;
+
 		m_Size = Array.m_Size;
 		m_Capacity = Array.m_Capacity;
-		delete[] m_Array;
+
 		m_Array = new T[m_Capacity];
+
 		memcpy(m_Array, Array.m_Array, sizeof(T) * m_Size);
 	}
-	void operator [] (int Index)
+
+	T& operator [](int Index)	const
 	{
-		if (Index < 0 || Index >= m_Size) return;
+		if (Index < 0 || Index >= m_Size)
+			assert(false);
+
 		return m_Array[Index];
 	}
-	void clear()
-	{
-		for (int i = 0; i < m_Size; i++)
-		{
 
-		}
-	}
 	bool erase(const T& data)
 	{
-		int Index = -1;
-		for (int i = 0; i < m_Size; i++)
+		int	Index = -1;
+
+		for (int i = 0; i < m_Size; ++i)
 		{
 			if (m_Array[i] == data)
 			{
@@ -76,23 +131,33 @@ public :
 				break;
 			}
 		}
-		if (Index == -1) return false;
-		return erase(Index);
+
+		if (Index == -1)
+			return false;
+
+		return eraseIndex(Index);
 	}
-	bool erase(const int Index)
+
+	bool eraseIndex(int Index)
 	{
-		if (Index < 0 || Index >= m_Size) return false;
-		for (int i = Index; i < m_Size; i++)
+		if (Index < 0 || Index >= m_Size)
+			return false;
+
+		for (int i = Index; i < m_Size - 1; ++i)
 		{
 			m_Array[i] = m_Array[i + 1];
 		}
+
 		--m_Size;
+
+		return true;
 	}
-	void sort((*pFunc)(const T&, const T&))
+
+	void sort(bool (*pFunc)(const T&, const T&))
 	{
-		for (int i = 0; i < m_Size - 1; i++)
+		for (int i = 0; i < m_Size - 1; ++i)
 		{
-			for (int j = i + 1; j < m_Size; j++)
+			for (int j = i + 1; j < m_Size; ++j)
 			{
 				if (pFunc(m_Array[i], m_Array[j]))
 				{
@@ -104,4 +169,3 @@ public :
 		}
 	}
 };
-
--- DesignPattern/Practice/Practice/main.cpp
+++ DesignPattern/Practice/Practice/main.cpp
@@ -1,9 +1,50 @@
-/*
-32 bit ?
-- 일반적으로 레지스터도 32bit
 
-왜냐하면, 32bit 보다 작으면
-하나의 데이터를 
-레지스터 안에 한번에 담을 수 없으니까 
+#include <iostream>
+#include "Array.h"
+#include <time.h>
 
-*/
+bool SortInt(const int& Left, const int& Right)
+{
+	return Left > Right;
+}
+
+int main()
+{
+
+	srand((unsigned int)time(0));
+	rand();
+
+	CArray<int>	Arr1;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		Arr1.push_back(rand());
+	}
+
+	// 복사생성자
+	CArray<int>	Arr2 = Arr1;
+	CArray<int>	Arr3;
+
+	// 대입연산자
+	Arr3 = Arr1;
+
+	//Arr1.erase(8);
+
+	int	Size = Arr1.size();
+	for (int i = 0; i < Size; ++i)
+	{
+		std::cout << Arr1[i] << std::endl;
+	}
+
+	std::cout << "Sort" << std::endl;
+
+	Arr1.sort(SortInt);
+
+	Size = Arr1.size();
+	for (int i = 0; i < Size; ++i)
+	{
+		std::cout << Arr1[i] << std::endl;
+	}
+
+	return 0;
+}
