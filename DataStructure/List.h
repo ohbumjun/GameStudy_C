75711edba098c1519b44652c324d5ab11c96e671
ohbumjun <beomjun0638@hanmail.net>
2021-11-25 11:42:22 +0900

    Graph

--- DataStructure/List.h
+++ DataStructure/List.h
@@ -1,127 +1,126 @@
 #include<assert.h>
 
 template<typename T>
-class CMergeSort
+class CEdge
 {
-public :
-	CMergeSort()
+	template<typename T>
+	friend class CGraph;
+
+	template<typename  T>
+	friend class CGraphNode;
+
+private :
+	CEdge()
+	{
+		m_Node = nullptr;
+	}
+	~CEdge(){}
+private :
+	class CGraphNode<T>* m_Node;
+};
+
+template<typename T>
+class CGraphNode
+{
+	template<typename T>
+	friend class CGraph;
+private :
+	CGraphNode()
 	{
 		m_Size = 0;
-		m_Capacity = 8;
-		m_Array = new T[m_Capacity];
-		m_Func = SortFunction;
+		m_Capacity = 1;
+		m_EdgeArray = new CEdge<T>*[m_Capacity];
+		m_Visit = false;
 	}
-	~CMergeSort()
+	~CGraphNode()
 	{
-		delete[] m_Array;
+		for (int i = 0; i < m_Size; i++)
+		{
+			delete m_EdgeArray[i];
+		}
+		delete[] m_EdgeArray;
 	}
 private :
+	CEdge<T>** m_EdgeArray;
 	int m_Size;
 	int m_Capacity;
-	T* m_Array;
-	T* m_CopyArray;
-	bool (*pFunc)(const T&, const T&);
-public :
-	void push(const T& data)
+	T m_Data;
+	bool m_Visit;
+private :
+	void AddEdge(CGraphNode<T>* Node)
 	{
 		if (m_Size == m_Capacity)
 		{
 			m_Capacity *= 2;
-			T* Array = new T[m_Capacity];
-			memcpy(Array, m_Array, sizeof(T) * m_Size);
-			delete[] m_Array;
-			m_Array = Array;
+			CEdge<T>** Array = new CEdge<T>*[m_Capacity];
+			memcpy(Array, m_EdgeArray, sizeof(CEdge<T>*) * m_Size);
 
-			delete[] m_CopyArray;
-			m_CopyArray = new T[m_Capacity];
+			delete[] m_EdgeArray;
+			m_EdgeArray = Array;
 		}
-		m_Array[m_Size] = data;
+		CEdge<T>* Edge = new CEdge<T>;
+		Edge->m_Node = Node;
+
+		m_EdgeArray[m_Size] = Edge;
 		++m_Size;
 	}
-	void push(T* Array, int Count)
+};
+
+template<typename T>
+class CGraph
+{
+public :
+	CGraph()
 	{
-		if (m_Capacity < Count)
-		{
-			m_Capacity = Count;
-			delete[] m_Array;
-			delete[] m_CopyArray;
-			m_Array = new T[m_Capacity];
-			m_CopyArray = new T[m_Capacity];
-		}
-		for (int i = 0; i < Count; i++)
-		{
-			m_Array[i] = Array[i];
-		}
-		m_Size = Count;
+		m_Size = 0;
+		m_Capacity = 4;
+		m_NodeArray = new CGraphNode<T>*[m_Capacity];
 	}
-	void Sort()
+	~CGraph()
 	{
-		MergeSort(0, m_Size - 1, m_Array);
-	}
-	void Sort(T* Array, int Count)
-	{
-		MergeSort(0, Count - 1, Array);
-	}
-	void MergeSort(int Left, int Right, T* Array)
-	{
-		if (Left < Right)
+		for (int i = 0; i < m_Size; i++)
 		{
-			int Mid = (Left + Right) / 2;
-			MergeSort(Left, Mid, Array);
-			MergeSort(Mid+1, Right, Array);
-			Merge(Left, Mid, Right, Array);
+			delete m_NodeArray[i];
 		}
-	}
-	void Merge(int Left, int Mid, int Right, T* Array)
-	{
-		int Low = Left;
-		int High = Mid + 1;
-		int Pivot = Left;
-		// 둘다 정렬 
-		while (Low <= Mid && High <= Right)
-		{
-			if (m_Func(Array[Low], Array[High]))
-			{
-				m_CopyArray[Pivot] = Array[High];
-				++High;
-				++Pivot;
-			}
-			else
-			{
-				m_CopyArray[Pivot] = Array[Low];
-				++Low;
-				++Pivot;
-			}
-		}
-		// 왼쪽 정렬이 끝났을 때
-		if (Low > Mid)
-		{
-			// 오른쪽을 마저 정렬해준다
-			for (int i = High; i <= Right; i++)
-			{
-				m_CopyArray[Pivot] = Array[i];
-				Pivot++;
-			}
-		}
-		// 오른쪽 정렬이 끝났을 때
-		else
-		{
-			// 왼쪽을 마저 정렬해준다
-			for (int i = Low; i <= Mid; i++)
-			{
-				m_CopyArray[Pivot] = Array[i];
-				Pivot++;
-			}
-		}
-		// 원래 배열에 복사해서 넣어준다
-		for (int i = Left; i <= High; i++)
-		{
-			m_Array[i] = m_CopyArray[i];
-		}
+		delete[] m_NodeArray;
 	}
 private :
-	static bool SortFunction(const T& Left, const T& Right)
+	int m_Size;
+	int m_Capacity;
+	CGraphNode<T>** m_NodeArray;
+public :
+	void insert(const T& data)
 	{
-		return Left > Right;
+		if (m_Size == m_Capacity)
+		{
+			m_Capacity *= 2;
+			CGraphNode<T>** Array = new CGraphNode<T>*[m_Capacity];
+			memcpy(Array, m_NodeArray, sizeof(CGraphNode<T>*) * m_Size);
+			delete[] m_NodeArray;
+			m_NodeArray = Array;
+		}
+		CGraphNode<T>* Node = new CGraphNode<T>;
+		Node->m_Data = data;
+		m_NodeArray[m_Size] = Node;
+		++m_Size;
+	}
+
+	// 노드에 edge 를 추가해준다.
+	void AddEdge(const T& Src, const T& Dest)
+	{
+		CGraphNode<T>* SrcNode = nullptr;
+		CGraphNode<T>* DestNode = nullptr;
+		for (int i = 0; i < m_Size; i++)
+		{
+			if (m_NodeArray[i]->m_Data == Src)
+				SrcNode = m_NodeArray[i];
+			else if (m_NodeArray[i]->m_Data == Dest)
+				DestNode = m_NodeArray[i];
+			if (SrcNode && DestNode)
+				break;
+		}
+		if (!SrcNode || !DestNode) return;
+		SrcNode->AddEdge(DestNode);
+		DestNode->AddEdge(SrcNode);
 	}
 };
