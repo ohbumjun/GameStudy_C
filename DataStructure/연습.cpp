617fbfddc1d2ccdbb1d5e1c35e16d34c938c99fc
ohbumjun <beomjun0638@hanmail.net>
2022-01-22 20:17:40 +0900

    아스키코드와 유니코드

--- DataStructure/연습.cpp
+++ DataStructure/연습.cpp
@@ -1,50 +1,27 @@
-// 가상함수의 호출 관계에서 보인 특성, 
-// 즉, 해당 함수 호출 시, 함수가 가상함수라면 포인터의 자료형을 기반으로 호출대상을 결정하지 않고
-// 포인터 변수가 실제로 가리키는 객체를 참조하여, 호출의 대상을 결정하는 형태
+#include <iostream>
 
-// 문장은 같은데 , 결과는 다른 특성
-// 다형성이란 하나의 타입에 여러 객체를 대입할 수 있는 성질
-#include<iostream>
+template <typename T>
+void wrapper(T&& u) {
+	g(std::forward<T>(u));
+}
 
-using namespace std;
+class A {};
 
-class First
-{
-public :
-    virtual void func()
-	{
-		cout << "First" << endl;
-	}
-};
+void g(A& a) { std::cout << "좌측값 레퍼런스 호출" << std::endl; }
+void g(const A& a) { std::cout << "좌측값 상수 레퍼런스 호출" << std::endl; }
+void g(A&& a) { std::cout << "우측값 레퍼런스 호출" << std::endl; }
 
-class Second : public First
-{
-public :
-    void func()
-    {
-    	cout << "Second" << endl;
-	}
-};
+int main() {
+	A a;
+	const A ca;
 
-class Third : public Second 
-{
-public :
-    void func()
-    {
-    	cout << "Third" << endl;
-	}
-};
+	std::cout << "원본 --------" << std::endl;
+	g(a);
+	g(ca);
+	g(A());
 
-int main()
-{
-    First* fptr = new First;
-    fptr->func();
-    delete fptr;
-    
-    fptr = new Second;
-    fptr->func();
-    delete fptr;
-
-    fptr = new Third;
-    fptr->func();
+	std::cout << "Wrapper -----" << std::endl;
+	wrapper(a);
+	wrapper(ca);
+	wrapper(A());
 }
