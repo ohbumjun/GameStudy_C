63b94bc0c248f607b05ab6f2a1a31355f0d11be4
ohbumjun <beomjun0638@hanmail.net>
2022-09-24 15:30:54 +0900

    Binary Search Tree

--- DataStructure/DS_Binary_Tree/main.cpp
+++ DataStructure/DS_Binary_Tree/main.cpp
@@ -66,7 +66,7 @@ int main()
 	iter = tree.Find(500);
 
 	if (iter == tree.end())
-		std::cout << "Ã£´Â µ¥ÀÌÅÍ°¡ ¾ø½À´Ï´Ù." << std::endl;
+		std::cout << "Ã£ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Í°ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ï´ï¿½." << std::endl;
 
 	else
 	{
@@ -100,7 +100,7 @@ int main()
 	for (iter = tree.begin(); iter != tree.end(); ++iter)
 	{
 		std::cout << "key : " << iter->first << " value : " <<
-			iter->second << std::endl;
+			iter->second << std::endl;//
 	}
 
 	return 0;
