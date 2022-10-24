a68cb2f5939ae71a24416f7b6b6640424e76692d
ohbumjun <beomjun0638@hanmail.net>
2022-10-24 22:00:44 +0900

    람다, 클로저

--- Practice/RandomPractice/RandomPractice/main.cpp
+++ Practice/RandomPractice/RandomPractice/main.cpp
@@ -1,10 +1,44 @@
-#include <random>
 #include <iostream>
-#include "B.h"
+#include <mutex>  // mutex 를 사용하기 위해 필요
+#include <thread>
+#include <vector>
+#include <string>
+#include <bitset>
 
-int main()
+using namespace std;
+
+struct A{
+   
+    // A(A&& a)
+    // {
+    //     cout << "Move copy" << endl;
+    // }
+   
+    int i = 0;
+};
+
+// A returnA(A a)
+
+A returnA()
 {
-    hello();
+    A a;
+    return a;
+}
+
+int main() {
+
+    // A pA;
+    A pB = returnA();
+
+   //auto two_i = [pA]() -> int {
+   //    
+   //    int a;
+   //    a = pA.i;
+   //
+   //    return a;
+   //};
+   //
+   //auto copyT = two_i;
 
     return 0;
 }
