d8c7b799f90f426609440580234be22fe2682ab3
ohbumjun <beomjun0638@hanmail.net>
2022-11-09 22:11:19 +0900

    로컬 IP, 공용 IP

--- Practice/RandomPractice/RandomPractice/main.cpp
+++ Practice/RandomPractice/RandomPractice/main.cpp
@@ -1,53 +1,35 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
-#include <stdio.h>
-#include <cstddef>
-#include <cstring>
+#include <mutex>  // mutex 를 사용하기 위해 필요
+#include <thread>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-struct Base
+int gstrcpy(char* src, char* dest)
 {
-public:
-	Base() { cout << "Base 생성자" << endl; }
-	virtual ~Base() { cout << "Base 소멸자" << endl; }
-public:
-	virtual void func1() { cout << "Base의 func1" << endl; }
-	virtual void func2() { cout << "Base의 func2" << endl; }
-	void func3() { cout << "Base의 func3" << endl; }
-public:
-	int iValue;
-	char cValue;
-};
+    int sum = 0;
 
-struct Child1 : Base
-{
-public:
-	Child1() { cout << "Child1 생성자" << endl; }
-	~Child1() { cout << "Child1 소멸자" << endl; }
-public:
-	virtual void func1() { cout << "Child1의 func1" << endl; }
-	virtual void func2() { cout << "Child1의 func2" << endl; }
-	void func3() { cout << "Child1의 func3" << endl; }
-public:
-	float fValue;
-};
-
-struct Child2 : Child1
-{
-public:
-	Child2() { cout << "Child2 생성자" << endl; }
-	~Child2() { cout << "Child2 소멸자" << endl; }
-public:
-	virtual void func1() { cout << "Child2의 func1" << endl; }
-	virtual void func2() { cout << "Child2의 func2" << endl; }
-	void func3() { cout << "Child2의 func3" << endl; }
-public:
-};
+    for (int i = 0; i < sizeof(dest); ++i)
+    {
+        src[i] = dest[i];
+    }
+    return sum;
+}
 
 int main() {
-	cout << sizeof(Base) << endl; // 16
-	cout << sizeof(Child1) << endl; // 24
-	cout << sizeof(Child2) << endl; // 24
+
+    char cArra[] = "hello1";
+    char cArra2[] = "hello2";
+
+    gstrcpy(cArra, cArra2);
+
+    cout << cArra << endl;
+
+    return 0;
 }
+
+// Insertion Sort 알고리즘을 사용해서 작은 값이
+// 앞으로 오도록 List를 정렬한 다음
+// head 를 반환하는 함수를 만드세요
+// (기본적으로 알고 있는 InsertionNode 의 반대를 출력한다고 생각하기)
