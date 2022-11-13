334918d6661898e895fd91086c10680d575084d7
ohbumjun <beomjun0638@hanmail.net>
2022-11-13 12:45:03 +0900

    파일시스템 OS

--- Practice/RandomPractice/RandomPractice/main.cpp
+++ Practice/RandomPractice/RandomPractice/main.cpp
@@ -1,48 +1,24 @@
 #include <iostream>
-#include <mutex>  // mutex 를 사용하기 위해 필요
-#include <thread>
-#include <vector>
-#include <string>
-#include <bitset>
 
 using namespace std;
 
-#define MAX 1000001
+class Base {
+    int x;
+public:
+    void set(int a) { x = a; }
+};
 
-bool IsDecimal[MAX];
+class Derived : public Base {
+    int x, y;
+public:
+    void set(int a) { x = a + 1; }
+};
 
-int main() {
+int main(void) {
 
-    memset(IsDecimal, true, sizeof(IsDecimal));
+    Derived D;
 
-    int M, N;
+    cout << sizeof(D) << endl;
 
-    cin >> M >> N;
-
-       cout << "hl" << endl;
-
-    int minN = min(M, N);
-    int maxN = max(M, N);
-
-    for (int i = 2; i < sqrt(1000001); ++i)
-    {
-        if (IsDecimal[i] == false)
-            continue;
-
-        for (int j = i; j < MAX; j += i)
-            IsDecimal[j] = false;
-
-    }
-
-    IsDecimal[2] = true;
-    IsDecimal[3] = true;
-
-    for (int i = minN; i <= maxN; ++i)
-    {
-        if (IsDecimal[i])
-            cout << i << " ";
-    }
-
-    cout << endl;
     return 0;
 }
