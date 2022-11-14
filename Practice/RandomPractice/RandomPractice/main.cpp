70ef675aec0df2bc1cf822b8430233fae59cd528
ohbumjun <beomjun0638@hanmail.net>
2022-11-14 11:18:55 +0900

    OS 유닉스 파일 시스템

--- Practice/RandomPractice/RandomPractice/main.cpp
+++ Practice/RandomPractice/RandomPractice/main.cpp
@@ -1,24 +1,51 @@
+#include <algorithm>
+#include <functional>
 #include <iostream>
+#include <string>
+#include <list>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-class Base {
-    int x;
-public:
-    void set(int a) { x = a; }
+template<typename T, typename Pred>
+void Erase(T& t, Pred P)
+{
+	auto iter = t.begin();
+	auto iterEnd = t.end();
+
+	for (; iter != iterEnd;)
+	{
+		if (P(iter->first))
+		{
+			iter = t.erase(iter);
+			continue;
+		}
+		++iter;
+	}
 };
 
-class Derived : public Base {
-    int x, y;
-public:
-    void set(int a) { x = a + 1; }
+struct PredP
+{
+	bool operator () (int elem)
+	{
+		return (elem & 1) == 0;
+	}
 };
 
-int main(void) {
+int main() {
+	
+	std::unordered_map<int, int> mapKeys = { {1,3},{2,4},{3,5} };
 
-    Derived D;
+	Erase(mapKeys, PredP());
 
-    cout << sizeof(D) << endl;
-
-    return 0;
+	for (const auto& [k, v] : mapKeys)
+	{
+		cout << "k,v : " << k << "," << v << endl;
+	}
+	
+	return 0;
 }
+
+// vec 내에서, 3이라는 값이 몇번째 Idx 에 위치하는지를 구하는 함수 ?
+// find 함수 + 그 외 함수
