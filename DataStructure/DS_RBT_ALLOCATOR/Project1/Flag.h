9d7bdf8af8e74d21b0e0eb34145e1d97a3d3476f
OBJ-1\obj <bumjunoh@com2us.com>
2023-06-22 20:58:47 +0900

    create RedBlackNode Class

--- DataStructure/DS_RBT_ALLOCATOR/Project1/Flag.h
+++ DataStructure/DS_RBT_ALLOCATOR/Project1/Flag.h
@@ -6,6 +6,11 @@
 #include <string.h>
 #include <bitset>
 
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
 typedef char int8;
 typedef unsigned char uint8;
 typedef short int16;
@@ -15,11 +20,35 @@ typedef unsigned int uint32;
 typedef unsigned int uint;
 typedef unsigned long ulong;
 
+/* detect Windows 95/98/NT/2000/XP/Vista/7 platform (윈도우 여부 파악) */
+#if (defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)) && !defined(__CYGWIN__)
+#if !defined(__WIN32__)
+#define __WIN32__
+#endif
+#endif
+
+/* detect x86 64 bit platform (32 혹은 64비트 시스템 여부 파악) */
+#if defined(__x86_64__) || defined(__ia64__) || defined(_M_X64)
+#if !defined(__X86__)
+#define __X86__
+#define __PLATFORM64__
+#endif
+#if !defined(__X86_64__)
+#define __X86_64__
+#define __PLATFORM64__
+#endif
+#endif
+
 #if defined(__WIN32__)
 	typedef long long int64;
 	typedef unsigned long long uint64;
 #endif
 
+#ifdef __PLATFORM64__
+	typedef uint64 uintptr;
+#else
+	typedef uint32 uintptr;
+#endif
 
 #define ALIGNOF(...) __alignof(__VA_ARGS__)
 
