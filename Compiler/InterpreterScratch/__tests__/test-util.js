1686bc7e59dc69c447f3f3570ff3cfb7607bc222
OBJ-1\obj <beomjun0638@hanmail.net>
2023-07-30 17:10:43 +0900

    Interpreter : custom parser generated from bnf grammar

---
+++ Compiler/InterpreterScratch/__tests__/test-util.js
@@ -0,0 +1,49 @@
+/*
+실제 s-expression format 을 통해서 test 를 위한 더 편한 format 을 사용할 것이다.
+참고로 Javascript 의 array 는 s-expression 과 1:1 대응관계를 이룬다.
+
+testUtil.test(`
+        (begin
+            (var x 10)
+            (var y 20)
+            (+ (* x 10) y))
+    `, 120);
+
+    즉, test method 는 s-expression 형태의 string 을 받아서
+    실행하는 형태가 될 것이다.
+
+    그리고 s-expression 을 parse 해서 AST format 으로 변경해주는
+    작은 parser 을 만들고자 한다.
+
+    "Syntax" tool 이라는 것을 사용할 것이다.
+    이는 Parser Generator 로 BNF grammer 로 부터
+    automatic parser 을 만들어내는 역할을 한다.
+
+
+즉,
+1) testUtil.test 함수에 s-expression 형태의 string 을 넘긴다.
+2) "Syntax tool" 을 활용한다. 
+   BNF 문법 -> Parser 를 만들어낸다. == evaParser.js
+3) evaParser.js 가 s-expression 을 parsing 해서 기존에 우리가 사용하던 
+ ['begin',
+            ['var', 'x', 10],
+            ['var', 'y', 20],
+            ['+', ['*', 'x', 'y'], 30],
+        ]
+  와 같은 형태를 만들어줄 것이다.
+4) 이를 그대로 eva.eval() 함수에 인자로 넘겨준다.
+*/
+
+const assert = require('assert');
+const evaParser = require('../parser/evaParser')
+
+function test(eva, code, expected)
+{
+    // 우선 string 을 실제 parse 한다.
+    const exp = evaParser.parse(code);
+    assert.strictEqual(eva.eval(exp), expected);
+}
+
+module.exports = {
+    test,
+}
--- Practice/RandomPractice/RandomPractice/main.cpp
+++ Practice/RandomPractice/RandomPractice/main.cpp
@@ -1,85 +1,39 @@
-//Locking mechanism to avoid race condition
 #include <iostream>
-#include <thread>
-#include <vector>
-#include <mutex>
 
-using namespace std;
+class Base {
+public:
+    virtual ~Base() {}
 
-class fridge
-{
-    int m_tomato;
-    std::mutex mutex;
-    public:
-    fridge()
-    {
-        m_tomato = 0;
+    void callVirtualFunction() const {
+        // Call the protected virtual function from a public function
+        virtualFunction();
     }
-    void addTomatoinFridge(int tomato)
-    {
-        // mutex.lock();
-        std::lock_guard<std::mutex> lockGuard(mutex);
-        for(int i=0; i<tomato; i++)
-        {
-            m_tomato++;
-            cout<<"callback memberfunc m_tomato is "<<m_tomato<<" and thread id is "<< std::this_thread::get_id() <<endl;
-        }
-        // mutex.unlock();
-    }
-    int getTomatofromFridge()
-    {
-        return m_tomato;
+
+private:
+    virtual void virtualFunction() const {
+        std::cout << "Base::virtualFunction()" << std::endl;
     }
 };
 
-int multiThreadTomatoCount()
-{
-    fridge friObj;
-	std::vector<std::thread> threadVec;
-	for(int i=0; i< 20; i++)
-    {
-        threadVec.push_back(std::thread(&fridge::addTomatoinFridge, &friObj, 10000));
+class Derived1 : public Base {
+public :
+    int a = 1;
+private:
+    void virtualFunction() const override {
+        std::cout << "Derived1::virtualFunction()" << std::endl;
     }
-	for(int i = 0; i<threadVec.size(); i++)
-	{
-		threadVec.at(i).join();
-	}
-    std::cout << friObj.getTomatofromFridge() << std::endl;
-    return friObj.getTomatofromFridge();
-}
+};
+int main() {
+    Derived1 *d1 = new Derived1;
+    Derived1 *d2 = new Derived1;
+    // d2->a = 4;
 
-int main()
-{
-    cout<<"In main func multithreaded "<<endl;
-	/*fridge friObj;
-	std::vector<std::thread> threadVec;
-	for(int i=0; i< 2; i++)
-    {
-        threadVec.push_back(std::thread(&fridge::addTomatoinFridge, &friObj, 250));
+    if (memcmp(d1, d2, sizeof(Derived1)) == 0) {
+        std::cout << "equal." << std::endl;
     }
-	for(int i = 0; i<threadVec.size(); i++)
-	{
-		threadVec.at(i).join();
-	}
-*/
-    // int cnt;
-	// for(int i=0; i<1; i++)
-    // {
-    //     //int tomatoCnt = multiThreadTomatoCount();
-    // 
-    //     if((cnt = multiThreadTomatoCount()) != 200000)
-    //     {
-    //         cout<<"error occurred and tomatoCnt is "<<cnt<<endl;
-    //     }
-    //     else
-    //     {
-    //         // cout<<"tomatoCnt is "<<cnt<<endl;
-    //    }
-    // }
-
-    uint64_t value = 9472753409530929250;
-    std::cout << value << std::endl;
-    return 0;
+    else {
+        std::cout << "not equal." << std::endl;
+    }
 
     return 0;
 }
