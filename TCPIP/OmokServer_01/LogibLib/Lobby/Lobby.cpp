4a861510c7db2e6552dc58ebf86934ca7c894b20
ohbumjun <beomjun0638@hanmail.net>
2024-06-15 13:57:19 +0900

    Analyze PacketProcessLobby Class

--- TCPIP/OmokServer_01/LogibLib/Lobby/Lobby.cpp
+++ TCPIP/OmokServer_01/LogibLib/Lobby/Lobby.cpp
@@ -1,11 +1,11 @@
 #include <algorithm>
 
-#include "NetLib/ILog.h"
-#include "NetLib/TcpNetwork.h"
-#include "Packet.h"
-#include "ErrorCode.h"
-#include "User.h"
-#include "Room.h"
+#include "../../NetLib/ILog.h"
+#include "../../NetLib/TcpNetwork.h"
+#include "../../Common/Packet.h"
+#include "../../Common/ErrorCode.h"
+#include "../User/User.h"
+#include "../Room/Room.h"
 #include "Lobby.h"
 
 using PACKET_ID = NCommon::PACKET_ID;
@@ -75,6 +75,7 @@ ERROR_CODE NLogicLib::Lobby::EnterUser(User* pUser)
 	}
 
 	// 들어갈 수 있는 User 가 존재하지 않는 다는 의미
+	// -> if (m_UserIndexDic.size() >= m_MaxUserCount) 조건과 같은 의미아닌가 ?
 	auto addRet = AddUser(pUser);
 	if (addRet != ERROR_CODE::NONE) {
 		return addRet;
@@ -84,26 +85,63 @@ ERROR_CODE NLogicLib::Lobby::EnterUser(User* pUser)
 
 ERROR_CODE NLogicLib::Lobby::LeaveUser(const int userIndex)
 {
-	return ERROR_CODE();
+	RemoveUser(userIndex);
+
+	// 음.. 이렇게 User 와 관련된 정보들을 m_UserindexDic, m_UserIDDic, 여러 형태로 관리하는 게
+	// 맞는 것인가 ?
+	auto pUser = FindUser(userIndex);
+
+	if (pUser == nullptr) {
+		return ERROR_CODE::LOBBY_LEAVE_USER_NVALID_UNIQUEINDEX;
+	}
+
+	pUser->LeaveLobby();
+
+	m_UserIndexDic.erase(pUser->GetIndex());
+	m_UserIDDic.erase(pUser->GetID().c_str());
+
+	return ERROR_CODE::NONE;
 }
 
 short NLogicLib::Lobby::GetUserCount()
 {
-	return 0;
+	return static_cast<short>(m_UserIndexDic.size());
 }
 
 NLogicLib::Room* NLogicLib::Lobby::CreateRoom()
 {
+	for (int i = 0; i < (int)m_RoomList.size(); ++i)
+	{
+		if (m_RoomList[i]->IsUsed() == false) {
+			return m_RoomList[i];
+		}
+	}
 	return nullptr;
 }
 
 NLogicLib::Room* NLogicLib::Lobby::GetRoom(const short roomIndex)
 {
-	return nullptr;
+	if (roomIndex < 0 || roomIndex >= m_RoomList.size()) {
+		return nullptr;
+	}
+
+	return m_RoomList[roomIndex];
 }
 
 void NLogicLib::Lobby::SendToAllUser(const short packetId, const short dataSize, char* pData, const int passUserindex)
 {
+	for (auto& pUser : m_UserIndexDic)
+	{
+		if (pUser.second->GetIndex() == passUserindex) {
+			continue;
+		}
+
+		if (pUser.second->IsCurDomainInLobby() == false) {
+			continue;
+		}
+
+		m_pRefNetwork->SendData(pUser.second->GetSessioIndex(), packetId, dataSize, pData);
+	}
 }
 
 NLogicLib::User* NLogicLib::Lobby::FindUser(const int userIndex)
@@ -137,4 +175,11 @@ ERROR_CODE NLogicLib::Lobby::AddUser(User* pUser)
 
 void NLogicLib::Lobby::RemoveUser(const int userIndex)
 {
+	auto findIter = std::find_if(std::begin(m_UserList), std::end(m_UserList), [userIndex](auto& lobbyUser) { return lobbyUser.pUser != nullptr && lobbyUser.pUser->GetIndex() == userIndex; });
+
+	if (findIter == std::end(m_UserList)) {
+		return;
+	}
+
+	findIter->pUser = nullptr;
 }
--- TCPIP/OmokServer_01/LogibLib/Packet/PacketProcessLobby.cpp
+++ TCPIP/OmokServer_01/LogibLib/Packet/PacketProcessLobby.cpp
@@ -13,4 +13,91 @@ using PACKET_ID = NCommon::PACKET_ID;
 
 namespace NLogicLib
 {
+	ERROR_CODE PacketProcess::LobbyEnter(PacketInfo packetInfo)
+	{
+		auto reqPkt = (NCommon::PktLobbyEnterReq*)packetInfo.pRefData;
+		NCommon::PktLobbyEnterRes resPkt;
+
+		auto [errorCode, pUser] = m_pRefUserMgr->GetUser(packetInfo.SessionIndex);
+
+		if (errorCode != ERROR_CODE::NONE) {
+			// 1) 유저가 없는 경우
+			// 2) user 가 Confirm 이 아닌 경우
+			resPkt.SetError(errorCode);
+			m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_ENTER_RES, sizeof(NCommon::PktLobbyEnterRes), (char*)&resPkt);
+			return errorCode;
+		}
+
+		if (pUser->IsCurDomainInLogIn() == false) 
+		{
+			// User 가 Login 상태가 아니라면
+			resPkt.SetError(ERROR_CODE::LOBBY_ENTER_INVALID_DOMAIN);
+			m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_ENTER_RES, sizeof(NCommon::PktLobbyEnterRes), (char*)&resPkt);
+			return ERROR_CODE::LOBBY_ENTER_INVALID_DOMAIN;
+		}
+
+		auto pLobby = m_pRefLobbyMgr->GetLobby(reqPkt->LobbyId);
+
+		if (pLobby == nullptr) 
+		{
+			// User 가 요청한 Lobby 가 존재하지 않는다면
+			resPkt.SetError(ERROR_CODE::LOBBY_ENTER_INVALID_LOBBY_INDEX);
+			m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_ENTER_RES, sizeof(NCommon::PktLobbyEnterRes), (char*)&resPkt);
+			return ERROR_CODE::LOBBY_ENTER_INVALID_LOBBY_INDEX;
+		}
+
+		ERROR_CODE enterRet = pLobby->EnterUser(pUser);
+
+		if (enterRet != ERROR_CODE::NONE) {
+			// 1) Lobby 최대 인원을 초과
+			// 2) 같은 User 존재
+			// 3) 들어갈 수 있는 User 가 없음... -> 1과 동일한 거 아닌가 ?
+			resPkt.SetError(enterRet);
+			m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_ENTER_RES, sizeof(NCommon::PktLobbyEnterRes), (char*)&resPkt);
+			return enterRet;
+		}
+
+		// User 를 Lobby 에 넣어주고
+		// 관련 res pkt 을 Lobby 입장 요청한 user Client 에게 보내준다.
+		resPkt.MaxUserCount = pLobby->MaxUserCount();
+		resPkt.MaxRoomCount = pLobby->MaxRoomCount();
+		m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_ENTER_RES, sizeof(NCommon::PktLobbyEnterRes), (char*)&resPkt);
+		return ERROR_CODE::NONE;
+	}
+
+	ERROR_CODE PacketProcess::LobbyLeave(PacketInfo packetInfo)
+	{
+		NCommon::PktLobbyLeaveRes resPkt;
+
+		auto [errorCode, pUser] = m_pRefUserMgr->GetUser(packetInfo.SessionIndex);
+
+		if (errorCode != ERROR_CODE::NONE) {
+			resPkt.SetError(errorCode);
+			m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_LEAVE_RES, sizeof(NCommon::PktLobbyLeaveRes), (char*)&resPkt);
+			return errorCode;
+		}
+
+		if (pUser->IsCurDomainInLobby() == false) {
+			resPkt.SetError(ERROR_CODE::LOBBY_LEAVE_INVALID_DOMAIN);
+			m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_LEAVE_RES, sizeof(NCommon::PktLobbyLeaveRes), (char*)&resPkt);
+			return ERROR_CODE::LOBBY_LEAVE_INVALID_DOMAIN;
+		}
+
+		Lobby* pLobby = m_pRefLobbyMgr->GetLobby(pUser->GetLobbyIndex());
+		if (pLobby == nullptr) {
+			resPkt.SetError(ERROR_CODE::LOBBY_LEAVE_INVALID_LOBBY_INDEX);
+			m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_LEAVE_RES, sizeof(NCommon::PktLobbyLeaveRes), (char*)&resPkt);
+			return ERROR_CODE::LOBBY_LEAVE_INVALID_LOBBY_INDEX;
+		}
+
+		ERROR_CODE enterRet = pLobby->LeaveUser(pUser->GetIndex());
+		if (enterRet != ERROR_CODE::NONE) {
+			resPkt.SetError(enterRet);
+			m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_LEAVE_RES, sizeof(NCommon::PktLobbyLeaveRes), (char*)&resPkt);
+			return enterRet;
+		}
+
+		m_pRefNetwork->SendData(packetInfo.SessionIndex, (short)PACKET_ID::LOBBY_LEAVE_RES, sizeof(NCommon::PktLobbyLeaveRes), (char*)&resPkt);
+		return ERROR_CODE::NONE;
+	}
 }
